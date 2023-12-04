6834ad6a5835b29178763d09322882cc79819fb5
andreasfiedler <fiedler.a51@gmail.com>
2023-12-04 15:09:44 +0100

    Geänderte Version

---
+++ app/app/src/main/java/com/syntax_institut/whatssyntax/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.syntax_institut.whatssyntax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.syntax_institut.whatssyntax.data.Datasource
+import com.syntax_institut.whatssyntax.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //BottomNavigationView anbinden
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavBNV.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/syntax_institut/whatssyntax/SecondFragment.kt
@@ -0,0 +1,49 @@
+package com.syntax_institut.whatssyntax
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+package com.syntax_institut.whatssyntax
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.syntax_institut.whatssyntax.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Auslesen der Argumente
+        arguments?.let {
+
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //binding.textView2.text = ""
+    }
+
+}
