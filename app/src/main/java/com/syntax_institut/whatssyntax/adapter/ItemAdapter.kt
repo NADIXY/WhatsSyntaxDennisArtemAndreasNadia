26ff13ed183323e10b9271d4e712016e18135078
andreasfiedler <fiedler.a51@gmail.com>
2023-12-04 23:26:38 +0100

    Geänderte Version

--- app/src/main/java/com/syntax_institut/whatssyntax/adapter/ItemAdapter.kt
+++ app/src/main/java/com/syntax_institut/whatssyntax/adapter/ItemAdapter.kt
@@ -13,7 +13,7 @@ class ItemAdapter (
 
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    inner class ItemViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ItemViewHolder( binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
