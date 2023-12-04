94efc3e3d5533e4c9c73f2cc9a88633cd3fe8460
andreasfiedler <fiedler.a51@gmail.com>
2023-12-04 23:10:58 +0100

    Geänderte Version

---
+++ app/src/main/java/com/syntax_institut/whatssyntax/ItemAdapter.kt
@@ -0,0 +1,33 @@
+package com.syntax_institut.whatssyntax
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.syntax_institut.whatssyntax.data.model.Call
+import com.syntax_institut.whatssyntax.databinding.ListItemBinding
+
+class ItemAdapter (
+    private val context: Context,
+    private val dataset: List<Call>
+
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    inner class ItemViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
+
+
+
