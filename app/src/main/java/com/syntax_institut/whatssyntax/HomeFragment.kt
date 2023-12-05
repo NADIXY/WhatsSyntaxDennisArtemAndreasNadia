4ef72707ff10bea41161937236a9ade49e9be2d2
andreasfiedler <fiedler.a51@gmail.com>
2023-12-05 07:55:59 +0100

    Geänderte Version

--- app/src/main/java/com/syntax_institut/whatssyntax/HomeFragment.kt
+++ app/src/main/java/com/syntax_institut/whatssyntax/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
 import com.syntax_institut.whatssyntax.databinding.FragmentHomeBinding
 import com.syntax_institut.whatssyntax.databinding.FragmentSecondBinding
 
