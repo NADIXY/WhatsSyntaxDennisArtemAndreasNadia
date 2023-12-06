05a189b658dc94a2434c088b4ba2168dca149c55
dennisrogalski <schueler1a2a@gmail.com>
2023-12-06 15:56:52 +0100

    Dennis 6.12.23

---
+++ app/src/main/java/com/syntax_institut/whatssyntax/adapter/CallAdapter.kt
@@ -0,0 +1,74 @@
+package com.syntax_institut.whatssyntax.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.syntax_institut.whatssyntax.R
+import com.syntax_institut.whatssyntax.StatusFragmentDirections
+import com.syntax_institut.whatssyntax.data.model.Call
+import com.syntax_institut.whatssyntax.data.model.Contact
+import com.syntax_institut.whatssyntax.databinding.ItemCallBinding
+import com.syntax_institut.whatssyntax.databinding.ListItemBinding
+
+class CallAdapter(
+
+    private val dataset: List<Call>
+
+) : RecyclerView.Adapter<CallAdapter.ItemViewHolder>() {
+
+    inner class ItemViewHolder(val binding: ItemCallBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = ItemCallBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val call = dataset[position]
+        val contact: Contact
+        holder.binding.tvCallName.text = call.contact.name
+        holder.binding.tvCallTime.text = call.contact.number
+        holder.binding.ivCallBenutzerbild.setImageResource(call.contact.image)
+
+
+// Überprüft ob Anruf entgegegenommen wurde und roter oder grüner Pfeil wird angezeigt
+        if (call.accepted) {
+            // Wenn der Status wahr ist, ändere die Pfeilfarbe auf grün und drehe ihn entsprechend
+            holder.binding.ivCallPfeil.setImageResource(R.drawable.icon_call_accepted)
+        } else {
+            // Wenn der Status falsch ist, ändere die Pfeilfarbe auf rot und drehe ihn entsprechend
+            holder.binding.ivCallPfeil.setImageResource(R.drawable.icon_call_missed)
+        }
+
+
+
+
+        //Überprüft ob eingehender oder ausgehender Anrufe und Pfeil wird gedreht
+
+        //image Pfeil drehen
+
+
+
+
+
+
+
+
+
+
+        holder.binding.contactCard.setOnClickListener {
+            holder.itemView.findNavController()
+                .navigate(StatusFragmentDirections.actionStatusFragmentToContactFragment(position))
+
+
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
